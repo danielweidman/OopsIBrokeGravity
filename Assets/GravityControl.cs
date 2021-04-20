c0b21802a7c16ce28870a6e7f95057197706d9ae
Daniel Weidman <dww60@case.com>
2021-04-20 08:55:23 -0500

    Add main project files

---
+++ Assets/GravityControl.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GravityControl : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public GameObject panel;
+
+    public bool enabled = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enabled)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                Physics2D.gravity = new Vector2(-9.8f, 0);
+                rb2d.gravityScale = 1;
+                panel.gameObject.SetActive(true);
+                StartCoroutine(DelayGravity());
+            }
+
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                Physics2D.gravity = new Vector2(9.8f, 0);
+                rb2d.gravityScale = 1;
+                panel.gameObject.SetActive(true);
+                StartCoroutine(DelayGravity());
+            }
+
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                Physics2D.gravity = new Vector2(0, 9.8f);
+                rb2d.gravityScale = 1;
+                panel.gameObject.SetActive(true);
+                StartCoroutine(DelayGravity());
+            }
+
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                Physics2D.gravity = new Vector2(0, -9.8f);
+                rb2d.gravityScale = 1;
+                panel.gameObject.SetActive(true);
+                StartCoroutine(DelayGravity());
+            }
+        }
+        
+    }
+
+    private IEnumerator DelayGravity() {
+        enabled = false;
+        yield return new WaitForSeconds(1f);
+        panel.gameObject.SetActive(false);
+        enabled = true;
+    }
+}
---
+++ Assets/MovingDrone.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingDrone : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public Vector3 position1 = new Vector3(-5, 0, 0);
+    public Vector3 position2 = new Vector3(5, 0, 0);
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(position1, position2, Mathf.PingPong(Time.time * speed, 1.0f));
+    }
+}

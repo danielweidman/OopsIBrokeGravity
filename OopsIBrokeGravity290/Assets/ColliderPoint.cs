c2276df907c2d1c0a918debd24e77eabf7750d9e
Daniel Weidman <dww60@case.com>
2021-04-22 13:35:37 -0400

    add latest project package

---
+++ OopsIBrokeGravity290/Assets/ColliderPoint.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderPoint : MonoBehaviour
+{
+    [SerializeField] GameObject entering;
+    [SerializeField] float numberTargets;
+    [SerializeField] GameObject transitionPoint;
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == entering)
+        {
+            transitionPoint.GetComponent<TransitionPoint>().setCount(transitionPoint.GetComponent<TransitionPoint>().getCount() + 1);
+        }
+
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject == entering)
+        {
+            transitionPoint.GetComponent<TransitionPoint>().setCount(transitionPoint.GetComponent<TransitionPoint>().getCount() - 1);
+        }
+    }
+}
---
+++ OopsIBrokeGravity290/Assets/TransitionPoint.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.SceneManagement;
+
+public class TransitionPoint : MonoBehaviour
+{
+    [SerializeField] private string newLevel;
+    [SerializeField] GameObject entering;
+    [SerializeField] float numberTargets;
+    private float count;
+
+    public float getCount()
+    {
+        return this.count;
+    }
+
+    public void setCount(float x)
+    {
+        this.count = x;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == entering)
+        {
+            count += 1;
+            if (count == numberTargets)
+            {
+                SceneManager.LoadScene(newLevel);
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject == entering)
+        {
+            count--;
+        }
+    }
+}

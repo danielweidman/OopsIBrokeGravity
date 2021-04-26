deb69541bdefeb0a2bb1095f72da362c573e52cc
Daniel Weidman <dww60@case.com>
2021-04-26 13:34:25 -0400

    physics changes re: rotation and friction

--- OopsIBrokeGravity290/Assets/MovingDrone.cs
+++ OopsIBrokeGravity290/Assets/MovingDrone.cs
@@ -2,14 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class MovingDrone : MonoBehaviour
 {
     public float speed = 1.0f;
     public Vector3 position1 = new Vector3(-5, 0, 0);
     public Vector3 position2 = new Vector3(5, 0, 0);
+    private Rigidbody2D rb;
 
+
+    void Start()
+    {
+        //rb = GetComponentInChildren<Rigidbody2D>();
+        //rb.AddForce(new Vector3(10.0f, 0, 0));
+        rb = GetComponentInChildren<Rigidbody2D>();
+
+    }
     void Update()
     {
-        transform.position = Vector3.Lerp(position1, position2, Mathf.PingPong(Time.time * speed, 1.0f));
+        //
+        rb.MovePosition(Vector3.Lerp(position1, position2, Mathf.PingPong(Time.time * speed, 1.0f)));
     }
 }

7730f35e358cd0dd8131bf3b7d5fec72221d560c
lizwat <emw119@case.edu>
2021-04-27 02:23:16 -0400

    added toggle switch for gravity

--- OopsIBrokeGravity290/Assets/GravityControl.cs
+++ OopsIBrokeGravity290/Assets/GravityControl.cs
@@ -8,12 +8,18 @@ public class GravityControl : MonoBehaviour
     private Rigidbody2D rb2d;
     public GameObject panel;
 
-    public bool enabled = true;
+    public bool enabled;
+
+    public void setEnabled(bool value)
+    {
+        enabled = value;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        enabled = true;
     }
 
     // Update is called once per frame
@@ -53,10 +59,41 @@ public class GravityControl : MonoBehaviour
                 StartCoroutine(DelayGravity());
             }
         }
-        
+        else if (!enabled)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                Physics2D.gravity = new Vector2(0.0f, 0);
+                rb2d.gravityScale = 0.0f;
+                panel.gameObject.SetActive(true);
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                Physics2D.gravity = new Vector2(0.0f, 0);
+                rb2d.gravityScale = 0.0f;
+                panel.gameObject.SetActive(true);
+            }
+
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                Physics2D.gravity = new Vector2(0.0f, 0);
+                rb2d.gravityScale = 0.0f;
+                panel.gameObject.SetActive(true);
+            }
+
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                Physics2D.gravity = new Vector2(0.0f, 0);
+                rb2d.gravityScale = 0.0f;
+                panel.gameObject.SetActive(true);
+            }
+
+        }
+
     }
 
-    private IEnumerator DelayGravity() {
+    private IEnumerator DelayGravity()
+    {
         enabled = false;
         yield return new WaitForSeconds(0.5f);
         panel.gameObject.SetActive(false);
---
+++ OopsIBrokeGravity290/Assets/ToggleSwitch.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+
+public class ToggleSwitch : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField] private bool _isOn = false;
+    public bool isOn
+    {
+        get
+        {
+            return _isOn;
+        }
+    }
+
+    public GameObject blue;
+    public GameObject green;
+
+
+    [SerializeField] private RectTransform toggleIndicator;
+    [SerializeField] private Image backgroundImage;
+    private Color onColor = Color.green;
+    private Color offColor = Color.red;
+    private float offX;
+    private float onX;
+    [SerializeField] private float tweenTime = 0.25f;
+
+
+    public delegate void ValueChanged(bool value);
+    public event ValueChanged valueChanged;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offX = toggleIndicator.anchoredPosition.x;
+        onX = (backgroundImage.rectTransform.rect.width - toggleIndicator.rect.width)*0.5f;
+
+    }
+
+    private void OnEnable()
+    {
+        Toggle(isOn);
+    }
+
+
+    public void Toggle(bool value)
+    {
+        if(value != isOn)
+        {
+            _isOn = value;
+            /* if (GameObject.Find("Orange Block") != null)
+             {
+                 GameObject orange = GameObject.Find("Orange Block");
+                 orange.GetComponent<GravityControl>().setEnabled(false);
+             } **/
+            if (value == true)
+            {
+                blue.GetComponent<GravityControl>().setEnabled(true);
+                green.GetComponent<GravityControl>().setEnabled(true);
+            }
+            else if (value == false)
+            {
+                blue.GetComponent<GravityControl>().setEnabled(false);
+                green.GetComponent<GravityControl>().setEnabled(false);
+            }
+
+
+            ToggleColor(isOn);
+            MoveIndicator(isOn);
+            if(valueChanged != null)
+            {
+                valueChanged(isOn);
+            }
+        }
+        else
+        {
+      
+            /*if (GameObject.Find("Orange Block") != null)
+            {
+                GameObject orange = GameObject.Find("Orange Block");
+                orange.GetComponent<GravityControl>().setEnabled(true);
+            } **/
+
+
+        }
+    }
+
+    private void ToggleColor(bool value)
+    {
+        if (value)
+            backgroundImage.DOColor(onColor, tweenTime);
+        else
+            backgroundImage.DOColor(offColor, tweenTime);
+    }
+
+    private void MoveIndicator(bool value)
+    {
+        if (value)
+            toggleIndicator.DOAnchorPosX(onX, tweenTime);
+        else
+            toggleIndicator.DOAnchorPosX(offX, tweenTime);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Toggle(!isOn);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

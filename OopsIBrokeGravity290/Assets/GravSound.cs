adaebeed018cbe2f7bc936bd322a53fcc8aa2f56
nky4 <nky4@case.edu>
2021-05-03 19:18:09 -0400

    Adjusted sound levels, added gravity sound effect, added title screen, instructions screen, endscreen

---
+++ OopsIBrokeGravity290/Assets/GravSound.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravSound : MonoBehaviour
+{
+    AudioSource gravSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gravSound = GetComponent<AudioSource>();
+        enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enabled)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                gravSound.Play();
+                StartCoroutine(DelayGravity());
+            }
+
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                gravSound.Play();
+                StartCoroutine(DelayGravity());
+            }
+
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                gravSound.Play();
+                StartCoroutine(DelayGravity());
+            }
+
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                gravSound.Play();
+                StartCoroutine(DelayGravity());
+            }
+        }
+    }
+
+    private IEnumerator DelayGravity()
+    {
+        enabled = false;
+        yield return new WaitForSeconds(0.5f);
+        enabled = true;
+    }
+}
---
+++ OopsIBrokeGravity290/Assets/TitleScreenButtons.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenButtons : MonoBehaviour
+{
+    public string newLevel;
+    public string instructions;
+    public string credits;
+    public string titlescreen;
+
+    public void Play()
+    {
+        SceneManager.LoadScene(newLevel);
+    }
+
+    public void Instructions()
+    {
+        SceneManager.LoadScene(instructions);
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene(credits);
+    }
+
+    public void Title()
+    {
+        SceneManager.LoadScene(titlescreen);
+    }
+
+    void Exit()
+    {
+        Application.Quit();
+    }
+
+}

12447abf2c55fbd176106907a40bbb4753c20078
nky4 <nky4@case.edu>
2021-05-09 16:46:26 -0400

    fixed quit buttons, added transition scenes with audio clip

---
+++ OopsIBrokeGravity290/Assets/ClickToContinue.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickToContinue : MonoBehaviour
+{
+    public string newLevel;
+
+    public void TaskOnClick()
+    {
+        SceneManager.LoadScene(newLevel);
+    }
+}
